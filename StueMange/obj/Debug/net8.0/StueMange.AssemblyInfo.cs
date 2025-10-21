1c8097e461feeb2f9d6455528d62657c6b2e85b8
moM3aty <mo.m3aty@gmail.com>
2025-10-21 22:22:29 +0300

    StueMange

--- StueMange/obj/Debug/net8.0/StueMange.AssemblyInfo.cs
+++ StueMange/obj/Debug/net8.0/StueMange.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StueMange")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25b19ff8e93c705086e189fab71681c5e71178b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StueMange")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StueMange")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
